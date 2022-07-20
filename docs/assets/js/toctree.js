ca50c38e7b418a24cb8f164e289b1e6431545fde
JV conseil <contact@jv-conseil.net>
2022-07-20 22:47:18 +0200

    Update toctree.js

--- docs/assets/js/toctree.js
+++ docs/assets/js/toctree.js
@@ -1,14 +1,14 @@
 $(function () {
     $(".markdown-body h2, .markdown-body h3").each(function () {
-        $("toctree nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '') + "'>" + $(this).text() + "</a></li>");
+        $(".toctree nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '') + "'>" + $(this).text() + "</a></li>");
         $(this).attr("id", $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, ''));
-        $("toctree nav ul li:first-child a").parent().addClass("active");
+        $(".toctree nav ul li:first-child a").parent().addClass("active");
     });
 
-    $("toctree nav ul li").on("click", "a", function (event) {
+    $(".toctree nav ul li").on("click", "a", function (event) {
         var position = $($(this).attr("href")).offset().top - 190;
         $("html, body").animate({ scrollTop: position }, 400);
-        $("toctree nav ul li a").parent().removeClass("active");
+        $(".toctree nav ul li a").parent().removeClass("active");
         $(this).parent().addClass("active");
         event.preventDefault();
     });
