d7a6bd45dcfd1effac5f11765797cd6f9831ca50
JV conseil <contact@jv-conseil.net>
2023-02-09 18:50:16 +0100

    :copyright:

--- .bash/osx/bash_alias.sh
+++ .bash/osx/bash_alias.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
 #
-# author        : JV conseil – Internet Consulting
+# author        : JV-conseil
 # credits       : JV-conseil
-# licence       : BSD 3-Clause License.
-# copyright     : Copyright (c) 2022, JV conseil – Internet Consulting,
-#                 All rights reserved.
-#======================================================================
+# licence       : BSD 3-Clause License
+# copyright     : Copyright (c) 2022-2023 JV-conseil
+#                 All rights reserved
+#====================================================
 
 
 
